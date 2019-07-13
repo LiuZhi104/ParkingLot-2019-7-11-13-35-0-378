567e6e31839db3733c65f8f6521b9519b3f40b39
LIUJE7 <LIUJE7@oocl.cpm>
2019-07-13 23:30:47 +0800

    [Jeffrey][ implement:customer of getErrorMessege method test:should return please provide your parking ticket when customer does not provide ticket]

--- src/main/java/com/thoughtworks/tdd/Customer.java
+++ src/main/java/com/thoughtworks/tdd/Customer.java
@@ -6,4 +6,8 @@ public class Customer {
     public String getMessege(Car fetchCar) {
         return "Unrecognized parking ticket.";
     }
+
+    public String getErrorMessege(Car fetchCar) {
+        return  "Please provide your parking ticket.";
+    }
 }
--- src/test/java/com/thoughtworks/tdd/ParkingCarStoryTwoTest.java
+++ src/test/java/com/thoughtworks/tdd/ParkingCarStoryTwoTest.java
@@ -20,4 +20,21 @@ public class ParkingCarStoryTwoTest {
         Assertions.assertSame("Unrecognized parking ticket.",actulMessege);
 
     }
+    @Test
+    public void should_retrun_please_provide_your_parking_ticket_when_customer_does_not_provide_ticket(){
+        //given
+        Car car=new Car();
+        Parkinglot parkinglot=new Parkinglot();
+        Parkingboy parkingboy=new Parkingboy(parkinglot);
+        Customer customer=new Customer();
+        // when
+        parkingboy.park(car);
+        Ticket ticket=new Ticket();
+        ticket=null;
+        Car fetchCar=parkingboy.fetchCar(ticket);
+        String actulMessege=customer.getErrorMessege(fetchCar);
+        //then
+        Assertions.assertSame("Please provide your parking ticket.",actulMessege);
+
+    }
 }
