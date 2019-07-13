c7a7f7c64fd8a057be2e55a2b49263d28eec2b80
LIUJE7 <LIUJE7@oocl.cpm>
2019-07-13 22:15:35 +0800

    [Jeffrey][implemt: park and fetchCar method; test:should return multiple cars when parking ticket back to parking boy to fetch car]

--- src/test/java/com/thoughtworks/tdd/ParkingCarStoryOneTest.java
+++ src/test/java/com/thoughtworks/tdd/ParkingCarStoryOneTest.java
@@ -16,22 +16,22 @@ public class ParkingCarStoryOneTest {
         //then
         Assertions.assertSame(car,fetchCar);
     }
-//    @Test
-//    public void should_return_multiple_cars_when_parking_ticket_back_to_parking_boy_to_fetch_car(){
-//        //given
-//        Car firstCar=new Car();
-//        Car secondCar=new Car();
-//        Parkinglot parkinglot=new Parkinglot();
-//        Ticket ticket= new Ticket();
-//        Parkingboy parkingboy=new Parkingboy(parkinglot);
-//        // when
-//        Ticket ticket=parkingboy.park(parkinglot);
-//        Car fetchFirstCar=parkingboy.fetchCar(ticket);
-//        Car fetchSecondCar=parkingboy.fetchCar(ticket);
-//        //then
-//        Assertions.assertSame(firstCar,fetchFirstCar);
-//        Assertions.assertSame(secondCar,fetchSecondCar);
-//    }
+    @Test
+    public void should_return_multiple_cars_when_parking_ticket_back_to_parking_boy_to_fetch_car(){
+        //given
+        Car firstCar=new Car();
+        Car secondCar=new Car();
+        Parkinglot parkinglot=new Parkinglot();
+        Parkingboy parkingboy=new Parkingboy(parkinglot);
+        // when
+        Ticket firstTicket=parkingboy.park(firstCar);
+        Ticket secondTicket=parkingboy.park(secondCar);
+        Car fetchFirstCar=parkingboy.fetchCar(firstTicket);
+        Car fetchSecondCar=parkingboy.fetchCar(secondTicket);
+        //then
+        Assertions.assertSame(firstCar,fetchFirstCar);
+        Assertions.assertSame(secondCar,fetchSecondCar);
+    }
 //    @Test
 //    public void should_return_exception_when_wrong_or_does_not_give_ticket_back_to_parking_boy_to_fetch_car(){
 //        //given
