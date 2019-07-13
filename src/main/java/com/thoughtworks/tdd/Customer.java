2980f226dee0a0efd2767bf8d1f336cb6374f7cd
LIUJE7 <LIUJE7@oocl.cpm>
2019-07-13 23:20:54 +0800

    [Jeffrey][ implement:customer of getMessege method test:should retrun unrecognized parking ticket when customer gives wrong ticket]

---
+++ src/main/java/com/thoughtworks/tdd/Customer.java
@@ -0,0 +1,9 @@
+package com.thoughtworks.tdd;
+
+public class Customer {
+
+
+    public String getMessege(Car fetchCar) {
+        return "Unrecognized parking ticket.";
+    }
+}
--- src/test/java/com/thoughtworks/tdd/ParkingCarStoryTwoTest.java
+++ src/test/java/com/thoughtworks/tdd/ParkingCarStoryTwoTest.java
@@ -1,4 +1,23 @@
 package com.thoughtworks.tdd;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ParkingCarStoryTwoTest {
+    @Test
+    public void should_retrun_unrecognized_parking_ticket_when_customer_gives_wrong_ticket(){
+        //given
+        Car car=new Car();
+        Parkinglot parkinglot=new Parkinglot();
+        Parkingboy parkingboy=new Parkingboy(parkinglot);
+        Customer customer=new Customer();
+        // when
+        parkingboy.park(car);
+        Ticket ticket=new Ticket();
+        Car fetchCar=parkingboy.fetchCar(ticket);
+        String actulMessege=customer.getMessege(fetchCar);
+        //then
+        Assertions.assertSame("Unrecognized parking ticket.",actulMessege);
+
+    }
 }
