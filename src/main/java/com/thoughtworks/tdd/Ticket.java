00e2852141320d7fac328b9fa4d8fea0eb6c2285
LIUJE7 <LIUJE7@oocl.cpm>
2019-07-14 17:46:28 +0800

    [Jeffrey][  test:should return car when parking ticket back to parking boy to fetch car muiltiple car]

--- src/main/java/com/thoughtworks/tdd/Ticket.java
+++ src/main/java/com/thoughtworks/tdd/Ticket.java
@@ -1,5 +1,9 @@
 package com.thoughtworks.tdd;
 
 public class Ticket {
+    private static int parkinglotId;
 
+    public int getParkinglotId() {
+        return parkinglotId;
+    }
 }
--- src/test/java/com/thoughtworks/tdd/ParkingCarStoryThreeTest.java
+++ src/test/java/com/thoughtworks/tdd/ParkingCarStoryThreeTest.java
@@ -1,4 +1,42 @@
 package com.thoughtworks.tdd;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
 public class ParkingCarStoryThreeTest {
+    @Test
+    public void should_return_car_when_parking_ticket_back_to_parking_boy_to_fetch_car_muiltiple_car() {
+        //given
+        Car car = new Car();
+        Parkinglot firstParkinglot = new Parkinglot();
+        Parkingboy parkingboy = new Parkingboy(firstParkinglot);
+
+        // when
+        List<Parkinglot> parkinglots = new ArrayList<Parkinglot>();
+        if (parkinglots.isEmpty()) {
+            Ticket ticket = parkingboy.park(car);
+            Car fetchCar = parkingboy.fetchCar(ticket);
+            //then
+            Assertions.assertSame(car, fetchCar);
+        }
+    }
+
+    @Test
+    public void should_return_car_when_parking_ticket_back_to_parking_boy_to_fetch_car_muiltiple_car_statify_1_and_2() {
+        //given
+        Car car = new Car();
+        Parkinglot firstParkinglot = new Parkinglot();
+        Parkingboy parkingboy = new Parkingboy(firstParkinglot);
+
+        // when
+        List<Parkinglot> parkinglots = new ArrayList<Parkinglot>();
+        if (parkinglots.isEmpty()) {
+            Ticket ticket = parkingboy.park(car);
+            Car fetchCar = parkingboy.fetchCar(ticket);
+            //then
+            Assertions.assertSame(car, fetchCar);
+        }
+    }
 }
